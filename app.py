b70535360ec9cb708514f790ff380361c116fab5
caffeicsatyam <satyamiscoding@gmail.com>
2026-03-27 00:20:24 +0530

    final commit

--- app.py
+++ app.py
@@ -229,4 +229,5 @@ def all_books():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host='0.0.0.0', port=port)
